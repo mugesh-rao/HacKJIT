e032a7d628aa73e7e28a8e60333d9dc33db5af88
MUgesh Rao <mugeshraoego@gmail.com>
2023-08-13 19:05:13 +0530

    some changes

--- script/index.js
+++ script/index.js
@@ -31,3 +31,63 @@ var swiper = new Swiper(".swiper", {
       }
     }
   });
+
+
+  window.addEventListener('scroll', () => {
+    const homeSectionHeight = document.getElementById('home').offsetHeight; // Height of the home section
+    const otherSections = document.querySelectorAll('.section'); // Other sections
+    const navbar = document.querySelector('.navbar'); // Navigation bar
+  
+    // Check if the user has scrolled beyond the home section
+    if (window.pageYOffset > homeSectionHeight) {
+      navbar.classList.add('hide-navbar'); // Hide the navbar
+  
+      // Hide other sections (assuming you have a class 'hide-section' to hide sections)
+      otherSections.forEach(section => {
+        section.classList.add('hide-section');
+      });
+    } else {
+      navbar.classList.remove('hide-navbar'); // Show the navbar
+  
+      // Show other sections
+      otherSections.forEach(section => {
+        section.classList.remove('hide-section');
+      });
+    }
+  });
+
+  const mobileMenuButton = document.getElementById("mobileMenuButton");
+        const navbar = document.getElementById("navbar");
+
+        mobileMenuButton.addEventListener("click", () => {
+          navbar.classList.toggle("mobile-open");
+        });
+
+        
+  const btnOpenModalAiMl = document.querySelector('.btn-open-modal-ai-ml');
+  const btnOpenModalGameDev = document.querySelector('.btn-open-modal-game-dev');
+  const btnOpenModalIoT = document.querySelector('.btn-open-modal-iot');
+  const btnCloseModal = document.querySelectorAll('.btn-close-modal');
+  const modalOverlayAiMl = document.querySelector('.modal-overlay-ai-ml');
+  const modalOverlayGameDev = document.querySelector('.modal-overlay-game-dev');
+  const modalOverlayIoT = document.querySelector('.modal-overlay-iot');
+
+  btnOpenModalAiMl.addEventListener('click', () => {
+    modalOverlayAiMl.style.display = 'flex';
+  });
+
+  btnOpenModalGameDev.addEventListener('click', () => {
+    modalOverlayGameDev.style.display = 'flex';
+  });
+
+  btnOpenModalIoT.addEventListener('click', () => {
+    modalOverlayIoT.style.display = 'flex';
+  });
+
+  btnCloseModal.forEach(btn => {
+    btn.addEventListener('click', () => {
+      modalOverlayAiMl.style.display = 'none';
+      modalOverlayGameDev.style.display = 'none';
+      modalOverlayIoT.style.display = 'none';
+    });
+  });
